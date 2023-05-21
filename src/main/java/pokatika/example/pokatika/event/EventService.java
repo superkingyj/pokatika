8a931ff72e6be34be98060ddc43e0f75c53a5f8b
RoxyYujinKim <yujinkim0707@gmail.com>
2023-05-21 14:49:59 +0900

    feat: S3 파일 다운로드 기능 구현 중

--- src/main/java/pokatika/example/pokatika/event/EventService.java
+++ src/main/java/pokatika/example/pokatika/event/EventService.java
@@ -1,6 +1,11 @@
 package pokatika.example.pokatika.event;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import pokatika.example.pokatika.common.ApiException;
 import pokatika.example.pokatika.event.dto.EventInfoByNftDto;
@@ -10,18 +15,21 @@ import pokatika.example.pokatika.nft.NftRepository;
 import pokatika.example.pokatika.participants.Participants;
 import pokatika.example.pokatika.participants.ParticipantsRepository;
 
-
 import java.time.format.DateTimeFormatter;
 
 import static pokatika.example.pokatika.common.StatusMessage.*;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EventService {
 
     private final ParticipantsRepository participantsRepository;
     private final EventRepository eventRepository;
     private final NftRepository nftRepository;
+    private final AmazonS3 amazonS3;
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
 
     /**
      * 월렛 주소를 입력받아 해당하는 지갑 주소, 이벤트 이미지, 순번을 넘겨줌
@@ -36,7 +44,10 @@ public class EventService {
                 .orElseThrow(() -> new ApiException(EVENT_NOT_FOUND));
 
         eventRepository.updateCount(event.getId());
-        byte[] remakeImage = remakeImage(event.getImage());
+
+        validateFileExists(event.getImageUrl());
+
+//        remakeImage(event.getImageUrl());
 
         return EventInfoByWalletDto.builder()
                 .walletAddress(participants.getWalletAddress())
@@ -47,7 +58,7 @@ public class EventService {
                 .endDate(event.getEndDate().format(DateTimeFormatter.ofPattern("yy.MM.dd hh:mm")))
                 .eventTitle(event.getEventTitle())
                 .address(event.getAddress())
-                .nftImage(remakeImage)
+                .nftImage(null)
                 .build();
     }
 
@@ -58,7 +69,7 @@ public class EventService {
      */
     public EventInfoByNftDto getEventInfoByNft(String nftTokenId){
         Nft nft = nftRepository.findByTokenId(nftTokenId)
-                .orElseThrow(() -> new ApiException(NFT_NOF_FOUND));
+                .orElseThrow(() -> new ApiException(NFT_NOT_FOUND));
 
         Event event = eventRepository.findEventById(nft.getEvent().getId())
                 .orElseThrow(() -> new ApiException(EVENT_NOT_FOUND));
@@ -72,8 +83,23 @@ public class EventService {
                 .build();
     }
 
-    private byte[] remakeImage(byte[] image){
-        byte[] remakeImage = new byte[]{1, 2, 3};
-        return remakeImage;
+    // 아직 짜는 중
+    private byte[] remakeImage(String fileName) {
+        S3Object s3Object = amazonS3.getObject(bucket, fileName);
+        S3ObjectInputStream s3ObjectInputStream = s3Object.getObjectContent();
+
+//        try{
+//            return IOUtils.toByteArray(s3ObjectInputStream);
+//        }catch (IOException e){
+//
+//        }
+        return new byte[]{};
+
+    }
+
+    private void validateFileExists(String fileName) throws ApiException {
+        if(amazonS3.doesObjectExist(bucket, fileName)){
+            throw new ApiException(IMAGE_NOT_FOUND);
+        }
     }
 }
