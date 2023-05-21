d7ed8f17e1f4f5846cd6140fe8a4a9e71fba9440
RoxyYujinKim <yujinkim0707@gmail.com>
2023-05-21 11:16:07 +0900

    fix: fix update jpql query

--- src/main/java/pokatika/example/pokatika/event/EventRepository.java
+++ src/main/java/pokatika/example/pokatika/event/EventRepository.java
@@ -3,6 +3,7 @@ package pokatika.example.pokatika.event;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +16,6 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @Transactional
     @Modifying
     @Query("UPDATE Event e SET e.count = (e.count+1) WHERE e.id = :id")
-    void updateCount(Long id);
+    void updateCount(@Param("id") Long id);
     boolean existsById(Long id);
 }
