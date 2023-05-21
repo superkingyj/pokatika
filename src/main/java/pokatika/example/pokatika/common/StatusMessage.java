6ccc6c79e1c4e6d51a471ece90b86c7bc849969c
RoxyYujinKim <yujinkim0707@gmail.com>
2023-05-21 11:18:09 +0900

    add: add NFT_NOT_FOUND_ERROR, IMAGE_REMAKE_ERROR, IMAGE_NOT_FOUND

--- src/main/java/pokatika/example/pokatika/common/StatusMessage.java
+++ src/main/java/pokatika/example/pokatika/common/StatusMessage.java
@@ -16,9 +16,11 @@ public enum StatusMessage {
     // 404 NOT_FOUND
     PARTICIPANT_NOT_FOUND(NOT_FOUND, "이벤트 참여자를 찾을 수 없습니다."),
     EVENT_NOT_FOUND(NOT_FOUND, "이벤트를 찾을 수 없습니다."),
-    NFT_NOF_FOUND(NOT_FOUND, "NFT를 찾을 수 없습니다"),
+    NFT_NOT_FOUND(NOT_FOUND, "NFT를 찾을 수 없습니다"),
+    IMAGE_NOT_FOUND(NOT_FOUND, "이미지를 찾을 수 없습니다."),
 
     // 500 INTERNAL_SEVER_ERROR
+    IMAGE_REMAKE_ERROR(INTERNAL_SERVER_ERROR, "NFT 이미지 제작 중 문제가 발생했습니다."),
     ERROR(INTERNAL_SERVER_ERROR, "서버에서 알 수 없는 에러가 발생했습니다.");
 
     private final HttpStatus httpStatus;
