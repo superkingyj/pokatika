85b9e91c7c4c9b30824af44672e03685e5c34846
RoxyYujinKim <yujinkim0707@gmail.com>
2023-06-04 04:27:26 +0900

    feat: cors 설정

---
+++ src/main/java/pokatika/example/pokatika/config/WebConfig.java
@@ -0,0 +1,15 @@
+package pokatika.example.pokatika.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("api/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:1234", "http://52.53.205.89")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
