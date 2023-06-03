b0cb1c2e4f34adbcf06437b2fad5bd28dde71d18
RoxyYujinKim <yujinkim0707@gmail.com>
2023-06-04 03:44:02 +0900

    feat: trail 계정을 받아 nft 이미지를 넘겨주는 getEventInfoById 구현

--- src/main/java/pokatika/example/pokatika/event/EventService.java
+++ src/main/java/pokatika/example/pokatika/event/EventService.java
@@ -5,13 +5,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.*;
 import org.springframework.http.converter.BufferedImageHttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 import pokatika.example.pokatika.common.ApiException;
 import pokatika.example.pokatika.event.dto.EventInfoByNftDto;
-import pokatika.example.pokatika.event.dto.EventInfoByWalletDto;
+import pokatika.example.pokatika.event.dto.EventInfo;
 import pokatika.example.pokatika.event.dto.IpfsResponse;
 import pokatika.example.pokatika.nft.Nft;
 import pokatika.example.pokatika.nft.NftRepository;
@@ -23,6 +27,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.time.format.DateTimeFormatter;
 
@@ -34,6 +39,7 @@ public class EventService {
     private final ParticipantsRepository participantsRepository;
     private final EventRepository eventRepository;
     private final NftRepository nftRepository;
+
     @Value("${ipfs.api}")
     private String IPFS_API;
     @Value("${ipfs.key}")
@@ -41,10 +47,13 @@ public class EventService {
     @Value("${ipfs.gateway}")
     private String GATEWAY;
 
+    // 지금은 static한 값을 저장
     private static Font FONT;
+    private static String FONT_NAME = "SK_Pupok_Solid_400.ttf";
     private static int X = 10;
-    private static int Y = 25;
-    private static Color COLOR = Color.black;
+    private static int Y = 50;
+    private static float FONT_SIZE = 20f;
+    private static Color COLOR = new Color(34, 79, 195);
 
     private static ObjectMapper objectMapper = new ObjectMapper();
 
@@ -53,6 +62,22 @@ public class EventService {
         this.participantsRepository = participantsRepository;
         this.eventRepository = eventRepository;
         this.nftRepository = nftRepository;
+        initFont();
+    }
+
+    private void initFont(){
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+        Resource resource = resourceLoader.getResource("classpath:fonts/" + FONT_NAME);
+        InputStream inputStream = null;
+        try {
+            inputStream = resource.getInputStream();
+            FONT = Font.createFont(Font.TRUETYPE_FONT, inputStream);
+            FONT = FONT.deriveFont(FONT_SIZE);
+            inputStream.close();
+        } catch (IOException | FontFormatException e) {
+            log.error("폰트 읽어오기 실패");
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -60,37 +85,42 @@ public class EventService {
      * @param walletAddress 지갑 주소
      * @return EventInfoByWalletDto
      */
-    public EventInfoByWalletDto getEventInfoByWallet(String walletAddress) {
+    @Transactional
+    public EventInfo getEventInfoByWallet(String walletAddress) {
         // 참여자 체크
         Participants participants = participantsRepository.findParticipantsByWalletAddress(walletAddress)
                 .orElseThrow(() -> new ApiException(PARTICIPANT_NOT_FOUND));
-        log.info("참여자 체크 완료");
 
         // 이벤트 체크
         Event event = eventRepository.findEventById(participants.getEvent().getId())
                 .orElseThrow(() -> new ApiException(EVENT_NOT_FOUND));
-        log.info("이벤트 체크 완료");
 
         eventRepository.updateCount(event.getId());
 
-        // 이벤트 이미지 체크
         if (!isExists(event.getCid())) { throw new ApiException(IMAGE_NOT_FOUND);}
-        log.info("이벤트 이미지 체크 완료");
+        log.info("1. 이벤트 이미지 체크");
 
-        // 이벤트 이미지 가져오기
         BufferedImage eventImage = getEventImageFromIpfs(event.getCid());
-        log.info("이벤트 이미지 가져오기 완료");
+        log.info("2. 이벤트 이미지 가져오기");
 
-        // NFT 이미지 생성 (이벤트 이미지에 트위터 핸들 작성)
         BufferedImage nftImage = writeString(eventImage, participants.getTwitterHandle(), FONT, COLOR, X, Y);
-        log.info("NFT 이미지 생성 완료");
+        log.info("3. NFT 이미지 생성 (이벤트 이미지에 트위터 핸들 작성)");
 
-        // IPFS에 NFT 이미지 저장
         String nfgImageCid = saveImageToIpfs(bufferedImageToByteArray(nftImage));
-        log.info("NFT 이미지 저장 완료");
+        log.info("4. IPFS에 NFT 이미지 저장");
 
-        // NFT 이미지를 담아 반환
-        return EventInfoByWalletDto.builder()
+        Nft nft = Nft.builder()
+                .event(event)
+                .participants(participants)
+                .cid(nfgImageCid)
+                .build();
+
+        participants.setNft(nft);
+
+        nftRepository.save(nft);
+        log.info("5. DB에 NFT cid 저장");
+
+        return EventInfo.builder()
                 .walletAddress(participants.getWalletAddress())
                 .twitterHandle(participants.getTwitterHandle())
                 .count(event.getCount())
@@ -103,13 +133,64 @@ public class EventService {
                 .build();
     }
 
+    @Transactional
+    public EventInfo getEventInfoById(Long eventId, String walletAddress) {
+
+        Event event = eventRepository.findEventById(eventId)
+                .orElseThrow(() -> new ApiException(EVENT_NOT_FOUND));
+
+        if (!isExists(event.getCid())) { throw new ApiException(IMAGE_NOT_FOUND);}
+        log.info("1. 이벤트 이미지 체크");
+
+        BufferedImage eventImage = getEventImageFromIpfs(event.getCid());
+        log.info("2. 이벤트 이미지 가져오기");
+
+        BufferedImage nftImage = writeString(eventImage, extractName(walletAddress), FONT, COLOR, X, Y);
+        log.info("3. NFT 이미지 생성 (이벤트 이미지에 월렛 주소 작성)");
+
+        String nfgImageCid = saveImageToIpfs(bufferedImageToByteArray(nftImage));
+        log.info("4. IPFS에 NFT 이미지 저장");
+
+        Participants participants = Participants.builder()
+                .walletAddress(walletAddress)
+                .event(event)
+                .isRsvp(true)
+                .build();
+        participantsRepository.save(participants);
+        log.info("5. DB에 참여자 저장");
+
+        Nft nft = Nft.builder()
+                .event(event)
+                .participants(participants)
+                .cid(nfgImageCid)
+                .build();
+
+        nftRepository.save(nft);
+        log.info("5. DB에 NFt cid 저장");
+
+        participants.setNft(nft);
+
+        // 6. NFT 이미지를 담아 반환
+        return EventInfo.builder()
+                .walletAddress(walletAddress)
+                .twitterHandle(null)
+                .count(event.getCount())
+                .eventId(event.getId())
+                .startDate(event.getStartDate().format(DateTimeFormatter.ofPattern("yy.MM.dd hh:mm")))
+                .endDate(event.getEndDate().format(DateTimeFormatter.ofPattern("yy.MM.dd hh:mm")))
+                .eventTitle(event.getEventTitle())
+                .address(event.getAddress())
+                .nftImageCid(nfgImageCid)
+                .build();
+    }
+
     /**
-     * 이벤트 상세보기: NFT 토큰 아이디를 받아 이벤트 정보를 넘겨줌
-     * @param nftTokenId NFT 토큰 아이디
+     * 이벤트 상세보기: NFT 이미지 cid를 받아 이벤트 정보를 넘겨줌
+     * @param nftCid NFT 이미지 cid
      * @return EventInfoByNftDto
      */
-    public EventInfoByNftDto getEventInfoByNft(String nftTokenId){
-        Nft nft = nftRepository.findByTokenId(nftTokenId)
+    public EventInfoByNftDto getEventInfoByNft(String nftCid){
+        Nft nft = nftRepository.findByCid(nftCid)
                 .orElseThrow(() -> new ApiException(NFT_NOT_FOUND));
 
         Event event = eventRepository.findEventById(nft.getEvent().getId())
@@ -217,5 +298,10 @@ public class EventService {
 
         HttpStatus statusCode = response.getStatusCode();
         return statusCode.is2xxSuccessful();
+    }
+
+    private String extractName(String walletAddress){
+        int dotIndex = walletAddress.indexOf('.');
+        return walletAddress.substring(0, dotIndex);
     }
 }
