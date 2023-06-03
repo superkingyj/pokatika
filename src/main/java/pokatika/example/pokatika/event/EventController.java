ef927942ad0b32b10909bf77b3e43ce294a86c6e
RoxyYujinKim <yujinkim0707@gmail.com>
2023-06-04 03:39:57 +0900

    feat: trial 계정 민팅시 사용되는 API 구현

--- src/main/java/pokatika/example/pokatika/event/EventController.java
+++ src/main/java/pokatika/example/pokatika/event/EventController.java
@@ -1,6 +1,7 @@
 package pokatika.example.pokatika.event;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,36 +9,50 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pokatika.example.pokatika.common.ApiResponse;
 import pokatika.example.pokatika.event.dto.EventInfoByNftDto;
-import pokatika.example.pokatika.event.dto.EventInfoByWalletDto;
+import pokatika.example.pokatika.event.dto.EventInfo;
 
 import static pokatika.example.pokatika.common.StatusMessage.SUCCESS;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("event")
+@RequestMapping("api/event")
+@Slf4j
 public class EventController {
 
     private final EventService eventService;
 
     /**
-     * 월렛 주소를 넘겨 받아 해당하는 지갑 주소, 이벤트 이미지, 순번을 넘겨줌
+     * RSVP 민팅시 사용: 월렛 주소를 넘겨 받아 해당하는 지갑 주소, 이벤트 이미지, 순번을 넘겨줌
      * @param walletAddress 월랫 주소
      * @return eventInfoDto 지갑_주소, 순번, NFT이미지
      */
     @GetMapping("/by-wallet/{walletAddress}")
     public ResponseEntity<?> eventInfoByWallet (@PathVariable String walletAddress) {
-        EventInfoByWalletDto eventInfoByWallet = eventService.getEventInfoByWallet(walletAddress);
-        return ApiResponse.successWitchBody(SUCCESS, eventInfoByWallet);
+        log.info("컨트롤러 진입");
+        EventInfo eventInfo = eventService.getEventInfoByWallet(walletAddress);
+        return ApiResponse.successWitchBody(SUCCESS, eventInfo);
     }
 
     /**
-     * NFT 토큰 아이디를 받아 해당 하는 이벤트 정보를 넘겨줌
-     * @param nftTokenId
+     * Trial 계정 민팅시 사용: 이벤트 아이디, 월렛 주소 넘겨 받아 월렛 주소, 이벤트 이미지, 순번을 넘겨줌
+     * @param id
      * @return
      */
-    @GetMapping("/by-nft/{nftTokenId}")
-    public ResponseEntity<?> eventInfoByNftToken (@PathVariable String nftTokenId) {
-        EventInfoByNftDto eventInfoByNft = eventService.getEventInfoByNft(nftTokenId);
+    @GetMapping("/by-id/{id}/{walletAddress}")
+    public ResponseEntity<?> eventInfo(@PathVariable Long id, @PathVariable String walletAddress){
+        log.info("컨트롤러 진입");
+        EventInfo eventInfo = eventService.getEventInfoById(id, walletAddress);
+        return ApiResponse.successWitchBody(SUCCESS, eventInfo);
+
+    }
+    /**
+     * NFT 이미지의 cid를 받아 해당 하는 이벤트 정보를 넘겨줌
+     * @param nftCid
+     * @return
+     */
+    @GetMapping("/by-nft/{nftCid}")
+    public ResponseEntity<?> eventInfoByNftToken (@PathVariable String nftCid) {
+        EventInfoByNftDto eventInfoByNft = eventService.getEventInfoByNft(nftCid);
         return ApiResponse.successWitchBody(SUCCESS, eventInfoByNft);
     }
 }
